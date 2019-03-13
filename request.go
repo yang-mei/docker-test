ad4a9434285d21c56d4dd19a9b6f5e9b40a4580c
yang.m <yang.m@sand.com.cn>
2019-03-13 15:35:53 +0800

    初始化项目版本

---
+++ request.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+        "net/http"
+)
+
+func main() {
+
+        http.HandleFunc("/v1/test", func(writer http.ResponseWriter,
+                request *http.Request) {
+                writer.Write([]byte("OK"))
+                return
+        })
+        http.HandleFunc("/v1/offline-acceptance/trans-type-and-status", func(writer http.ResponseWriter, request *http.Request) {
+                writer.Write([]byte("YES"))
+                return
+        })
+        http.ListenAndServe(":6116", nil)
+}
+
